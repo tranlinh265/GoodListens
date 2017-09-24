69c1ca9adfb2ce455b4d9702e0abb95016342eb0
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-09-24 17:12:00 +0700

    user profile and authen view complete

--- db/migrate/20170922032140_devise_create_users.rb
+++ db/migrate/20170922032140_devise_create_users.rb
@@ -2,8 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.text :intro
 
       ## Recoverable
       t.string   :reset_password_token
